80c731b2b949bc63e235264f0985b1f992c73368
Rashid <arrc3174@gmail.com>
2025-05-02 19:06:22 -0600

    agregue el frontend

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const cors = require('cors');
 const express = require('express');
+const path = require('path');  // Añadir este módulo
 const routes = require('../routes');
 
 const server = express();
+
+// Configuración existente
 server.use(cors());
 server.options('*', cors());
-
 server.use(express.json());
 
+// Servir archivos estáticos del frontend
+server.use(express.static(path.join(__dirname, '../public')));
+
+// Tus rutas API
 server.use('/api', routes);
 
-server.get('/', (req, res) => {
-    res.redirect('/api');
-  });
+// Redirección para SPA (Single Page Application)
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = server;
